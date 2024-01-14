dbd5b39a1e92b0bc401bedad7a947b15e9846b5b
YakovVazan <yakovvazan@gmail.com>
2024-01-14 15:37:31 +0200

    fix: chevron left for plug page is bigger now

--- src/components/Header/Nav/Nav.jsx
+++ src/components/Header/Nav/Nav.jsx
@@ -23,8 +23,8 @@ const Header = () => {
           ) : (
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
+              width="24"
+              height="24"
               fill="currentColor"
               className="bi bi-chevron-left"
               viewBox="0 0 16 16"
