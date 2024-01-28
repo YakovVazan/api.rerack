a10d5efea85b02efd2dd338a02954a4de5e0c520
YakovVazan <yakovvazan@gmail.com>
2024-01-28 09:03:55 +0200

    refactor: now many devices can connect to the local host in dev

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 
 const app = express();
+const port = process.env.PORT || 5174;
 
-app.listen(5174, () => {
-    console.log("listening on 5174");
-})
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is listening on port ${port}`);
+});
