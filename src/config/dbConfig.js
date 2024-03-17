ef546830683273f1e8b83f6bf249013d15d65c2a
YakovVazan <yakovvazan@gmail.com>
2024-03-17 10:36:38 +0200

    fix: moved secrets to .env

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -4,12 +4,15 @@ available on https://phpmyadmin.freedb.tech/index.php
  ********************************/
 
 import mysql from 'mysql2'
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const pool = mysql.createPool({
-  host: 'sql.freedb.tech',
-  user: 'freedb_YkvVzn',
-  password: 'jnY29TaN%QDWhca',
-  database: 'freedb_Rerack',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   waitForConnections: true,
   connectionLimit: 5,
   queueLimit: 0
